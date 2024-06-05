235a677a88cbf40237d127fa85dd86bc859841db
MasizaExpect <157204784+MasizaExpect@users.noreply.github.com>
2024-06-05 05:08:15 +0200

    Add files via upload

---
+++ chatBot/static/scripts/chat.js
@@ -0,0 +1,109 @@
+// Collapsible
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
+    });
+}
+
+function getTime() {
+    let today = new Date();
+    hours = today.getHours();
+    minutes = today.getMinutes();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    let time = hours + ":" + minutes;
+    return time;
+}
+
+// Gets the first message
+function firstBotMessage() {
+
+    let firstMessage = "🌟 Welcome to Masiza Expect NPO's AI Chat! 🚀  we're here to uplift our community.\n\n 1️⃣Education, Science, Agriculture, and Tech (DESAT): 📚🔬🌱\n   - Focus: Education, Science, Tech.\n\n2️⃣ Social Development, Sports, Arts, Culture, and Events (DSSACE): 🤝🏀🎨🎉\n   - Focus: Recruitment, Social Development, Sports, Arts, Culture, Events.\n\nReady for an adventure in knowledge, empowerment, and community upliftment?\n\n3️⃣What is expected from you ? \n\n4️⃣Membership Requirements.\n\n 💪💡 Ask us anything or share your thoughts! Let's make this chat unforgettable! 🌈✨";
+
+    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+
+    let time = getTime();
+
+    $("#chat-timestamp").append(time);
+    document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
+
+// Retrieves the response
+function getHardResponse(userText) {
+    let botResponse = getBotResponse(userText);
+    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+    $("#chatbox").append(botHtml);
+
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+}
+
+//Gets the text text from the input box and processes it
+function getResponse() {
+    let userText = $("#textInput").val();
+
+    if (userText == "") {
+        userText = "I love Code Palace!";
+    }
+
+    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    setTimeout(() => {
+        getHardResponse(userText);
+    }, 1000)
+
+}
+
+// Handles sending text via button clicks
+function buttonSendText(sampleText) {
+    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+
+    
+    setTimeout(() => {
+     getHardResponse(sampleText);
+    }, 1000)
+}
+
+function sendButton() {
+    getResponse();
+}
+
+function heartButton() {
+    buttonSendText("Heart clicked!")
+}
+
+// Press enter to send a message
+$("#textInput").keypress(function (e) {
+    if (e.which == 13) {
+        e.preventDefault()
+        getResponse();
+    }
+})
+
